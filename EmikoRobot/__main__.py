5e0348ec8daebbcf5288b85d6f6b77b0f048e5c4
x11fq <96665742+x11fq@users.noreply.github.com>
2022-01-10 22:59:09 +0500

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -229,7 +229,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            f"👋 Hi, I'm {dispatcher.bot.first_name}. Nice to meet You.",
+            f" Hoi, {dispatcher.bot.first_name}. is already started.",
             parse_mode=ParseMode.HTML
        )
 
