60ad1b9e1e23490d4c2bcbd5b8671356a2383a00
Senpaii <96665742+x11fq@users.noreply.github.com>
2022-02-12 21:03:37 +0500

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,13 +9,13 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/7971bd0a4530f2d571b43.jpg"
+PHOTO = "https://telegra.ph/file/d8471a7e6306f5447ee51.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Ruka Sarashina.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm GiyuBot.** \n\n"
   TEXT += "**I'm Working Properly** \n\n"
-  TEXT += f"**My Master : [x11fq](https://t.me/x11fq)** \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Ruka_Robot?start=help"), Button.url("Support", "https://t.me/RukaSupport_Chat")]]
+  TEXT += f"**My Master : [Eren](https://t.me/Euthanizer)** \n\n"
+  TEXT += "**Thanks For Adding Me Here**"
+  BUTTON = [[Button.url("Help", "https://t.me/Giyu_Superbot?start=help"), Button.url("Support", "https://t.me/GiyuSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
